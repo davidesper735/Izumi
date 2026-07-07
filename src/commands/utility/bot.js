6f0e8dec9205e36f2c1f3090532dd5d4b0f68e8a
David Esper <david.esper735@gmail.com>
2026-07-06 23:02:37 -0500

    Update bot.js

--- src/commands/utility/bot.js
+++ src/commands/utility/bot.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const os = require('os');
 
 module.exports = {
-  category: 'Diversión',
+  category: 'Utilidad',
   data: new SlashCommandBuilder()
     .setName('bot')
     .setDescription('Información del bot')
